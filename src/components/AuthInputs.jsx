a3da3b10ad5a700982e49d5b8880caf7e50f2cd6
RonanKer29 <kervella.ronan@hotmail.com>
2024-08-19 11:40:29 +0200

    nothing important (test)

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -24,7 +24,7 @@ const Input = styled.input`
   line-height: 1.5;
   background-color: #d1d5db;
   color: #374151;
-  border: 1px solid transparent;
+  border: 2px solid transparent;
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
