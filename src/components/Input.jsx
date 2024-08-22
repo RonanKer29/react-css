a963ccd8e16a4915212b8a7fafcd9a275a3c9df6
RonanKer29 <kervella.ronan@hotmail.com>
2024-08-22 16:59:46 +0200

    tailwind styles conditionally

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,31 +1,42 @@
-import { styled } from "styled-components";
+// import { styled } from "styled-components";
 
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
-`;
+// const Label = styled.label`
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   font-size: 0.75rem;
+//   font-weight: 700;
+//   letter-spacing: 0.1em;
+//   text-transform: uppercase;
+//   color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
+// `;
 
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({ $invalid }) => ($invalid ? "#fee2e2" : "#f3f4f6")};
-  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
-  border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "#6b7280")};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
+// const Input = styled.input`
+//   width: 100%;
+//   padding: 0.75rem 1rem;
+//   line-height: 1.5;
+//   background-color: ${({ $invalid }) => ($invalid ? "#fee2e2" : "#f3f4f6")};
+//   color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
+//   border: 1px ${({ $invalid }) => ($invalid ? "#f73f3f" : "#6b7280")};
+//   border-radius: 0.25rem;
+//   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+// `;
 
-export default function CustomInput({ label, invalid, ...props }) {
+export default function Input({ label, invalid, ...props }) {
+  let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase";
+  let inputClasses = "w-full px-3 py-2 leading-tight  border rounded shadow ";
+
+  if (invalid) {
+    labelClasses += " text-red-700";
+    inputClasses += " text-red-500 text-red-500 bg-red-100";
+  } else {
+    labelClasses += " text-gray-700";
+    inputClasses += " bg-blue-200 text-gray-700";
+  }
+
   return (
     <p>
-      <Label $invalid={invalid}>{label}</Label>
-      <Input $invalid={invalid} {...props} />
+      <label className={labelClasses}>{label}</label>
+      <input className={inputClasses} {...props} />
     </p>
   );
 }
