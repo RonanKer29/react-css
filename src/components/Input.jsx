6935ca0df25f9552b3323b296ec9c2d783bf7786
RonanKer29 <kervella.ronan@hotmail.com>
2024-08-22 13:25:30 +0200

    corrected invalid props

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,17 +7,16 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: ${(invalid) => (invalid ? "#6b7280" : "#f87171")};
+  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
 `;
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: ${(invalid) =>
-    invalid ? "1px solid black" : "1px solid transparent"};
+  background-color: ${({ $invalid }) => ($invalid ? "#fee2e2" : "#f3f4f6")};
+  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
+  border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "#6b7280")};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
