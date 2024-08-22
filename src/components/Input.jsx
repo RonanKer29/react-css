fd22862b64f112f1772e9a4630661c29db39505a
RonanKer29 <kervella.ronan@hotmail.com>
2024-08-22 17:19:33 +0200

    fixing tailwind version

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -26,11 +26,11 @@ export default function Input({ label, invalid, ...props }) {
   let inputClasses = "w-full px-3 py-2 leading-tight  border rounded shadow ";
 
   if (invalid) {
-    labelClasses += " text-red-700";
-    inputClasses += " text-red-500 text-red-500 bg-red-100";
+    labelClasses += " text-red-400";
+    inputClasses += " text-red-500 border-red-300 bg-red-100";
   } else {
-    labelClasses += " text-gray-700";
-    inputClasses += " bg-blue-200 text-gray-700";
+    labelClasses += " text-stone-300";
+    inputClasses += " bg-stone-300 text-gray-700";
   }
 
   return (
